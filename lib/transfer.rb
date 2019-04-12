cfb2e41509cac879414d827984830e062bc08fff
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-12 03:27:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,10 @@ class Transfer
   end
 
   def reverse_transfer
-
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
   end
 end
