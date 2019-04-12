0378a5aa03a45e25c6f482ea1895c6e0ac177109
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-12 03:12:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
-    @sender = sender
+    @sender = BankAccount.new(sender)
     @receiver = receiver
     @status = 'pending'
     @amount = amount
