c19fb0732080c06d93b9bb1f7d1ac3b563e8272c
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-12 03:29:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,8 @@ class Transfer
     if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
     else
       @sender.deposit( @amount * -1 )
       @receiver.deposit( @amount )
